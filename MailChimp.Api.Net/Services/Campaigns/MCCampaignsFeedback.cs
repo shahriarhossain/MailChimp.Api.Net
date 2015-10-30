f9fad41cafda15f501e5231fefd2223d2cf12c6b
Shahriar Hossain <shahriar_cse@live.com>
2015-10-30 20:11:12 +0600

    Campaign Feature Added

---
+++ MailChimp.Api.Net/Services/Campaigns/MCCampaignsFeedback.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MailChimp.Api.Net.Domain.Feedback;
+using MailChimp.Api.Net.Enum;
+using Newtonsoft.Json;
+
+namespace MailChimp.Api.Net.Services.Campaigns
+{
+    // ==================================================================================================================================
+    // AUTHOR      : Shahriar Hossain
+    // PURPOSE     : Post comments, reply to team feedback, and send test emails while you’re working together on a MailChimp campaign.
+    // ==================================================================================================================================
+    public class MCCampaignsFeedback
+    {
+        /// <summary>
+        /// Get feedback about a campaign
+        /// <param name="campaignId">Unique id for the campaign</param>
+        /// </summary>
+        public async Task<RootFeedback> GetCampaignFeedbackAsync(string campaignId)
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.feedback, SubTargetType.not_applicable, campaignId);
+
+            string content;
+            using (var client = new HttpClient())
+            {
+                Authenticate.ClientAuthentication(client);
+
+                content = await client.GetStringAsync(endpoint);
+            }
+
+            return JsonConvert.DeserializeObject<RootFeedback>(content);
+        }
+
+        /// <summary>
+        /// Get feedback about a campaign
+        /// <param name="campaignId">Unique id for the campaign</param>
+        /// <param name="feedback_id">Unique id for the feedback message.</param>
+        /// </summary>
+        public async Task<Feedback> GetSpecificFeedbackAsync(string campaignId, string feedback_id)
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.feedback, SubTargetType.not_applicable, campaignId, feedback_id);
+
+            string content;
+            using (var client = new HttpClient())
+            {
+                Authenticate.ClientAuthentication(client);
+
+                content = await client.GetStringAsync(endpoint);
+            }
+
+            return JsonConvert.DeserializeObject<Feedback>(content);
+        }
+
+        /// <summary>
+        /// Delete a campaign feedback message
+        /// <param name="campaignId">Unique id for the campaign</param>
+        /// <param name="feedback_id">Unique id for the feedback message.</param>
+        /// </summary>
+        public async Task<Feedback> DeleteSpecificFeedbackAsync(string campaignId, string feedback_id)
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.feedback, SubTargetType.not_applicable, campaignId, feedback_id);
+
+            HttpResponseMessage result;
+            using (var client = new HttpClient())
+            {
+                Authenticate.ClientAuthentication(client);
+
+                result = await client.DeleteAsync(endpoint);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MailChimp.Api.Net/Services/Campaigns/MCCampaignsOverview.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MailChimp.Api.Net.Domain.Campaigns;
+using MailChimp.Api.Net.Enum;
+using Newtonsoft.Json;
+
+namespace MailChimp.Api.Net.Services.Campaigns
+{
+    // ==================================================================================================================================================
+    // AUTHOR      : Shahriar Hossain
+    // PURPOSE     : Campaigns are how you send emails to your MailChimp list. Use the Campaigns API calls to manage campaigns in your MailChimp account.
+    // ===================================================================================================================================================
+    public class MCCampaignsOverview
+    {
+        /// <summary>
+        /// Get all campaigns
+        /// </summary>
+        public async Task<RootCampaign> GetCampaignsAsync()
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.not_applicable, SubTargetType.not_applicable);
+
+            string content;
+            using (var client = new HttpClient())
+            {
+                Authenticate.ClientAuthentication(client);
+
+                content = await client.GetStringAsync(endpoint);
+            }
+
+            return JsonConvert.DeserializeObject<RootCampaign>(content);
+        }
+
+        /// <summary>
+        /// Get information about a specific campaign
+        /// <param name="campaignId">Unique id for the campaign</param>
+        /// </summary>
+        public async Task<Campaign> GetCampaignsByIdAsync(string campaignId)
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.not_applicable, SubTargetType.not_applicable, campaignId);
+
+            string content;
+            using (var client = new HttpClient())
+            {
+                Authenticate.ClientAuthentication(client);
+
+                content = await client.GetStringAsync(endpoint);
+            }
+
+            return JsonConvert.DeserializeObject<Campaign>(content);
+        }
+
+        /// <summary>
+        /// Delete a campaign
+        /// <param name="campaignId">Unique id for the campaign</param>
+        /// </summary>
+        public async Task<HttpResponseMessage> DeleteCampaignsByIdAsync(string campaignId)
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.not_applicable, SubTargetType.not_applicable, campaignId);
+
+            HttpResponseMessage result;
+            using (var client = new HttpClient())
+            {
+                Authenticate.ClientAuthentication(client);
+
+                result = await client.DeleteAsync(endpoint);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Cancel a campaign
+        /// <param name="campaignId">Unique id for the campaign</param>
+        /// </summary>
+        public async Task<HttpResponseMessage> CancelCampaignsByIdAsync(string campaignId)
+        {
+            string endpoint = Authenticate.EndPoint(TargetTypes.campaigns, SubTargetType.action3, SubTargetType.not_applicable, campaignId);
+
+            HttpResponseMessage message = new HttpResponseMessage();
+            StringContent tempMsg= new StringContent("NOT IMPLEMENTED YET");
+            message.Content = tempMsg;
+            return message;
+           
+        }
+    }
+}
