bb0601b3e1946d396ce80a46aaceddcb1aff5bdb
Shahriar Hossain <shahriar_cse@live.com>
2015-12-08 16:16:24 +0600

    buggy

--- MailChimp.Api.Net/Helper/BundleCreation.cs
+++ MailChimp.Api.Net/Helper/BundleCreation.cs
@@ -14,22 +14,22 @@ namespace MailChimp.Api.Net.Helper
 {
     internal static class  BundleCreation
     {
-        RootBatch bundle = new RootBatch();
+        //RootBatch bundle = new RootBatch();
         
-        SingleOperation bundleElement = new SingleOperation();
+        //SingleOperation bundleElement = new SingleOperation();
       
-        public RootBatch CreateBundle(List<ListMemberBase> members, Method method, string endPoint )
-        { 
-            foreach (var item in members)
-            {
-                bundleElement.method = method.ToString();
-                bundleElement.path = endPoint;
-                bundleElement.body = JsonConvert.SerializeObject(item);
-                bundle.bundles.Add(bundleElement);
-            }
+        //public static RootBatch CreateBundle(List<ListMemberBase> members, Method method, string endPoint )
+        //{ 
+        //    foreach (var item in members)
+        //    {
+        //        bundleElement.method = method.ToString();
+        //        bundleElement.path = endPoint;
+        //        bundleElement.body = JsonConvert.SerializeObject(item);
+        //        bundle.bundles.Add(bundleElement);
+        //    }
 
-            return bundle;
-        }
+        //    return bundle;
+        //}
 
     }
 }
