9abdbf22447b97292ae03394c6abd8a7c583e09e
Shahriar Hossain <shahriar_cse@live.com>
2016-01-27 15:06:03 +0600

    File Parser Enhancement
    Easy Email Parser

---
+++ MailChimp.Api.Net/Helper/FileParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailChimp.Api.Net.Helper
+{
+    public class FileParser
+    {
+        public string EmailParser(string path)
+        {
+            string templateContent = "";
+
+            if (File.Exists(path))
+            {
+                try
+                {
+                    StreamReader reader = new StreamReader(path);
+                    templateContent = reader.ReadToEnd();
+                    reader.Close();
+                    
+                    //templateContent = templateContent.Replace("<##FirstName##>", registeredUser.Member.FirstName);
+                    return templateContent;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                } 
+            }
+            return templateContent;
+        }
+    }
+}
